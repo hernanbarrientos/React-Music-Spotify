61f69bf5344df87f4a0d1fa00100a6b82130c436
hernanbarrientos <hrn.2211@gmail.com>
2021-12-28 18:03:00 -0300

    useState do campo de busca - renderizando quando há valor no input

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { AlbumList } from "./Components/AlbumList";
 import { Header } from "./Components/Header";
 import { SearchBar } from "./SearchBar";
@@ -6,16 +6,35 @@ import { Default } from  './template/default'
 
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState<string>();
+
+  const handleSetTerm = (value: string) => {
+    setSearchTerm(value)
+  }
+
   return (
     <Default>
       <Header />
-      <SearchBar />
-      <AlbumList
-        title="Álbuns buscados recentemente"
+      <SearchBar
+        searchTerm={searchTerm}
+        handleSetTerm={handleSetTerm}
       />
       <AlbumList
-        title="Álbuns buscados recentemente"
+        title={
+          searchTerm
+          ? `Resultados encontrados para "${searchTerm}"`
+          : "Álbuns buscados recentemente"
+          }
+        
       />
+      {
+        !searchTerm &&
+        <AlbumList
+        title="Álbuns buscados recentemente"
+        />
+      }
+      
     </Default>
   );
 }
--- src/SearchBar/index.tsx
+++ src/SearchBar/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Container, Label, Input } from './styles';
 
+type SearchBarProps = {
+  handleSetTerm: (prop: string) => void;
+  searchTerm: string|undefined;
 
+}
 
-export function SearchBar() {
+export function SearchBar({handleSetTerm,  searchTerm}: SearchBarProps) {
   return (
     <Container>
       <Label>
         Busque por artistas, álbuns ou músicas
       </Label>
       <Input 
+        value={searchTerm}
+        onChange={(e)=>handleSetTerm(e.target.value) }
         placeholder="Comece a escrever..."/>
     </Container>
   )
