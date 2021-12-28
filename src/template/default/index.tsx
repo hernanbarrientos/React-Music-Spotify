bf14ccb38f7fcb29afba6ce53e14be803eb7d58c
hernanbarrientos <hrn.2211@gmail.com>
2021-12-28 00:38:11 -0300

    ajustes iniciais

---
+++ src/template/default/index.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode} from 'react';
+import {Container} from './styles';
+
+type ReactProps = {
+  children: ReactNode
+}
+export function Default({ children }: ReactProps) {
+  return (
+    <Container>
+      {children}
+    </Container>
+  )
+};
